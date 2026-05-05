0160d21181bc03b89641b78254146d04300de29f
rafiansagar <sagarworld420@gmail.com>
2026-05-05 11:25:01 +0600

    about section in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/layout/header/header";
 import Hero from "@/components/sections/home/hero";
 import Service from "@/components/sections/home/service";
+import About from "@/components/sections/home/about";
 
 export default function Home() {
     return (
         <>
-            {<Hero />}
+            <Header />
+            <Hero />
             <Service />
+            <About />
         </>
     );
 }
