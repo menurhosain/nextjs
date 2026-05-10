45005f33868849c70a0605e60daabd570dfab357
menurhosain <omithasan005@gmail.com>
2026-05-10 18:59:38 +0600

    increase height

--- src/components/sections/home/career.tsx
+++ src/components/sections/home/career.tsx
@@ -5,7 +5,7 @@ export default function Career() {
     return (
         <section className="section-padding h-[80vh] md:h-screen bg-[url('/career-section-bg.jpg')] bg-cover bg-bottom bg-no-repeat wax-w-[900px]">
             <div className="container h-full flex items-end">
-                <div className="md:w-[80%] lg:w-[55%] p-[30px] md:p-[50px] xl:p-[100px] bg-[url('/career-content-bg.jpg')] bg-cover bg-top bg-no-repeat">
+                <div className="md:w-[80%] lg:w-[60%] p-[30px] md:p-[50px] xl:p-[100px] bg-[url('/career-content-bg.jpg')] bg-cover bg-top bg-no-repeat">
                     <Section_Title
                         subtitle="Life at Sah"
                         title="Career with SAH"
