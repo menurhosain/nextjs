f86796bc119d83a2d372c802ca07d7c611024990
menurhosain <omithasan005@gmail.com>
2026-05-11 11:48:30 +0600

    added: every page hero section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,24 @@
+import Banner from "@/components/ui/banner";
+import Banner_Title from "@/components/ui/banner-title";
+
 export default function ContactPage() {
     return (
-        <main>
-            <h1>Contact</h1>
-        </main>
+        <Banner bg="/home-hero.mp4">
+            <Banner.Left class_name="">
+                <div className="flex flex-col justify-center">
+                    <Banner_Title
+                        subtitle="Reliable Business Partners"
+                        title=<>
+                            Let’s discuss your construction needs
+                            <br /> with experts
+                        </>
+                    />
+                </div>
+            </Banner.Left>
+
+            <Banner.Right>
+                <div></div>
+            </Banner.Right>
+        </Banner>
     );
 }
