533b260bd238d0437e69845f988834fdd3575485
menurhosain <omithasan005@gmail.com>
2026-05-11 10:42:42 +0600

    create all pages & add them on nav menus & added video on career section

---
+++ src/app/leadership/page.tsx
@@ -0,0 +1,7 @@
+export default function LeadershipPage() {
+    return (
+        <main>
+            <h1>Leadership</h1>
+        </main>
+    );
+}
---
+++ src/app/partner/page.tsx
@@ -0,0 +1,7 @@
+export default function PartnerPage() {
+    return (
+        <main>
+            <h1>Partner</h1>
+        </main>
+    );
+}
