ce980d6b23bd9452bfd42fb2c712700edc8c73ac
rstanvir82 <143778659+rstanvir82@users.noreply.github.com>
2026-05-13 15:25:49 +0600

    become a subcontractor page start

--- src/app/become-a-subcontractor/page.tsx
+++ src/app/become-a-subcontractor/page.tsx
@@ -3,22 +3,24 @@ import Banner_Title from "@/components/ui/banner-title";
 
 export default function BecomeASubcontrctorPage() {
     return (
-        <Banner bg="/home-hero.mp4">
-            <Left class_name="">
-                <div className="flex flex-col justify-center">
-                    <Banner_Title
-                        subtitle="Reliable Business Partners"
-                        title=<>
-                            Become a valued subcontractor in
-                            <br /> our network
-                        </>
-                    />
-                </div>
-            </Left>
+        <>
+            <Banner bg="/home-hero.mp4">
+                <Left class_name="">
+                    <div className="flex flex-col justify-center">
+                        <Banner_Title
+                            subtitle="Reliable Business Partners"
+                            title=<>
+                                Become a valued subcontractor in
+                                <br /> our network
+                            </>
+                        />
+                    </div>
+                </Left>
 
-            <Right>
-                <div></div>
-            </Right>
-        </Banner>
+                <Right>
+                    <div></div>
+                </Right>
+            </Banner>
+        </>
     );
 }
