eb3c75ee7cf2f267865516dbdfafc5d2f20406e6
menurhosain <omithasan005@gmail.com>
2026-05-18 12:04:03 +0600

    create 2 separate form for register applicant/contractor but same action

---
+++ src/app/register-contractor/page.tsx
@@ -0,0 +1,34 @@
+import RegisterContractorForm from "./register-form";
+import { Banner, Left, Right } from "@/components/ui/banner";
+import Banner_Title from "@/components/ui/banner-title";
+
+export default function RegisterContractorPage() {
+  return (
+    <>
+      <Banner bg="/home-hero.mp4">
+        <Left class_name="">
+          <div className="flex flex-col justify-center">
+            <Banner_Title
+              subtitle="Building Trust And Excellence"
+              title={<>
+                Building Trust Through <br /> Quality Construction <br /> Trust Through
+              </>}
+            />
+          </div>
+        </Left>
+
+        <Right>
+          <div></div>
+        </Right>
+      </Banner>
+      <div className="py-[150px] register-box flex items-center justify-center bg-gray-50 py-40 px-4">
+        <div className="w-full max-w-lg bg-white rounded-2xl shadow-md p-8">
+          <h1 className="text-2xl font-bold text-gray-900 mb-6">
+            Create a Contractor account
+          </h1>
+          <RegisterContractorForm />
+        </div>
+      </div>
+    </>
+  );
+}
