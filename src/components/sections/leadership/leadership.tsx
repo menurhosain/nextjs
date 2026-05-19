0fd31760111fcbee55a9a9788ef7c4743188ff9b
rstanvir82 <143778659+rstanvir82@users.noreply.github.com>
2026-05-19 11:41:11 +0600

    leadership page responsive

--- src/components/sections/leadership/leadership.tsx
+++ src/components/sections/leadership/leadership.tsx
@@ -47,24 +47,24 @@ export default function LeadershipSection() {
 
     return (
         <section className="section-padding bg-sah-light-4 bg-[url('/mosaic-patternt-bg.png')] bg-contain bg-bottom bg-no-repeat">
-            <div className="container pt-[140px] pb-[140px] border-x border-sah-light-3">
-                <div className="relative flex items-center p-[24px] pr-[135px] gap-[115px] rounded-[6px] bg-white overflow-hidden">
-                    <img className="absolute right-0 bottom-0" src="/construction-vector.png" alt="Vector" />
-                    <div className="rounded-[6px] overflow-hidden w-[522px] shrink-0 relative z-1">
-                        <img src="/team/mr-gm.png" alt="Mr. Sojan Varghese" />
+            <div className="container pt-[80px] lg:pt-[150px] pb-[140px] border-x border-sah-light-3 max-[1024px]:!px-4">
+                <div className="relative flex max-[768px]:flex-col xl:items-center p-[20px] sm:p-[24px] pr-[30px] 2xl:pr-[135px] gap-[20px] md:gap-[40px] 2xl:gap-[115px] rounded-[6px] bg-white overflow-hidden">
+                    <img className="absolute right-[-170px] 2xl:right-0 bottom-0" src="/construction-vector.png" alt="Vector" />
+                    <div className="rounded-[6px] overflow-hidden w-full md:w-[280px] lg:w-[350px] xl:w-[522px] shrink-0 relative z-1">
+                        <img src="/team/mr-gm.png" alt="Mr. Sojan Varghese" className="rounded-[6px]"/>
                     </div>
                     <div className="relative z-1">
                         <div className="text-[14px] leading-[28px] font-medium uppercase text-[#2B3944] mb-[13px]">Executive Leadership</div>
-                        <h3 className="font-geist text-[52px] leading-[62px] font-semibold text-[#2B3944] mb-[20px]">Mr. Sojan Varghese</h3>
-                        <p className="text-[17px] leading-[28px] font-normal text-sah-gray-1 mb-[36px]">Joined as GM, driving innovation, tech adoption, and expanding capabilities. Under his leadership, SAH diversified and scaled to 450 staff with a turnover of OMR 4,800,000 and began executing larger, more complex projects</p>
-                        <p className="text-[17px] leading-[28px] font-bold sah-dark-2">Leadership meets trust – SAH chosen to build the future.</p>
-                        <div className="flex items-center gap-[30px] mt-[62px]">
-                            <Link href="tel:+9689XXXXXXX" className="text-[17px] leading-[28px] font-normal sah-transition text-sah-gray-1 hover:text-sah-red">+968 9XXX XXXX</Link>
-                            <Link href="mailto:enquiries@sah.com" className="text-[17px] leading-[28px] font-normal sah-transition text-sah-gray-1 hover:text-sah-red">enquiries@sah.com</Link>
+                        <h3 className="font-geist text-[22px] sm:text-[30px] lg:text-[40px] xl:text-[46px] 2xl:text-[52px] leading-[30px] sm:leading-[40px] lg:leading-[50px] xl:leading-[56px] 2xl:leading-[62px] font-semibold text-[#2B3944] mb-[20px]">Mr. Sojan Varghese</h3>
+                        <p className="text-[15px] xl:text-[17px] leading-[24px] xl:leading-[28px] font-normal text-sah-gray-1 mb-[15px] xl:mb-[36px]">Joined as GM, driving innovation, tech adoption, and expanding capabilities. Under his leadership, SAH diversified and scaled to 450 staff with a turnover of OMR 4,800,000 and began executing larger, more complex projects</p>
+                        <p className="text-[15px] lg:text-[17px] leading-[28px] font-bold sah-dark-2">Leadership meets trust – SAH chosen to build the future.</p>
+                        <div className="flex max-[1024px]:flex-col lg:items-center gap-[0px] lg:gap-[30px] mt-[20px] xl:mt-[62px]">
+                            <Link href="tel:+9689XXXXXXX" className="text-[15px] lg:text-[17px] leading-[28px] font-normal sah-transition text-sah-gray-1 hover:text-sah-red">+968 9XXX XXXX</Link>
+                            <Link href="mailto:enquiries@sah.com" className="text-[15px] lg:text-[17px] leading-[28px] font-normal sah-transition text-sah-gray-1 hover:text-sah-red">enquiries@sah.com</Link>
                         </div>
                     </div>
                 </div>
-                <div className="grid grid-cols-3 gap-[30px] mt-[50px] mb-[140px]">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[30px] mt-[50px] mb-[80px] lg:mb-[150px]">
                     {teams.map((team, index) => (
                         <TeamCard
                             key={index}
