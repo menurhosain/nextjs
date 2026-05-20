c9c4d3be470552766c032f06917b1478df4fae3b
rstanvir82 <143778659+rstanvir82@users.noreply.github.com>
2026-05-20 10:36:53 +0600

    project details responsive complate

--- src/app/project-details/page.tsx
+++ src/app/project-details/page.tsx
@@ -83,8 +83,8 @@ export default function ProjectDetailsPage() {
 
     return (
         <>
-            <Banner bg="/home-hero.mp4">
-                <Left class_name="">
+            <Banner bg="/home-hero.mp4" class_name="lg:min-h-[auto] xl:min-h-[auto] md:min-h-[auto] 2xl:h-[100vh] py-18 2xl:py-0 max-[640px]:pb-[65px]">
+                <Left class_name="max-[640px]:pt-[70px]">
                     <div className="flex flex-col justify-center">
                         <Banner_Title subtitle="Insights Into Project Work" title={<>Complete Insights Into Project Design And Delivery</>} />
                     </div>
@@ -96,46 +96,46 @@ export default function ProjectDetailsPage() {
             </Banner>
 
             <section className="section-padding">
-                <div className="container mx-auto pt-[140px] pb-[150px] border-x border-sah-light-3">
-                    <div className="mb-[60px]">
+                <div className="container mx-auto pt-[80px] lg:pt-[150px] lg:pb-[150px] pb-[80px] border-x border-sah-light-3 max-[1024px]:!px-4">
+                    <div className="lg:mb-[60px] mb-[30px]">
                         <ProjectGallery images={projectImages} />
                     </div>
 
-                    <div className="grid grid-cols-[1fr_370px] place-items-start gap-x-[65px]">
+                    <div className="lg:grid 2xl:grid-cols-[1fr_370px] xl:grid-cols-[1fr_303px] lg:place-items-start gap-x-[30px] 2xl:gap-x-[65px]">
                         <div className="">
-                            <h3 className="text-[30px] font-bold font-geist">Building Construction Project</h3>
+                            <h3 className="text-[22px] sm:text-[30px] font-bold font-geist">Building Construction Project</h3>
                             <p className="text-[16px] leading-normal text-sah-gray-2 mt-2">At tempus aenean sapien torquent sed diam class efficitur mus morbi eros dictum quam augue ac laor eet ligula libero mi commodo nibh hac fermentum orci ad pharetra consequat justo duis turpis lorem elit dui consectetur magnis lacinia odio per placerat feugiat vestibulum volutpat mauris mollis primis imperdiet posu ere ex enim gravida cras congue</p>
                             <p className="text-[16px] leading-normal text-sah-gray-2 mt-6">pellentesque vulputate malesuada dictumst fames interdum cursus an te tellus porta ullamcorper accumsan non eu adipiscing  is the integer venenatis sagittis arcu curae finibus ridi culus aliquam velit lobortis senectus vitae sollicitudin sit consectetuer ultricies rutrum parturient pede nisi nascetur habitant netus quisque elementum inceptos nam felis penatibus feugiat</p>
-                            <h3 className="text-[30px] font-bold font-geist mt-8">Project Goals</h3>      
+                            <h3 className="text-[22px] sm:text-[30px] font-bold font-geist mt-8">Project Goals</h3>      
                             <p className="text-[16px] leading-normal text-sah-gray-2 mt-2">At tempus aenean sapien torquent sed diam class efficitur mus morbi eros dictum quam augue ac laor eet ligula libero mi commodo nibh hac fermentum orci ad pharetra consequat justo duis turpis lorem elit dui consectetur magnis lacinia odio per placerat congue vestibulum volutpat mauris mollis primis imperdiet posu ere ex enim gravida cras</p>
-                            <ul className="grid grid-cols-2 gap-4 w-150 mt-5">
+                            <ul className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full sm:w-150 mt-5">
                                 <li className="flex items-center">
-                                    <div className="bg-sah-red  rounded-full h-4 w-4 text-[10px] flex items-center justify-center">
+                                    <div className="bg-sah-red  rounded-full min-w-4 h-4 w-4 text-[10px] flex items-center justify-center">
                                         <ProjectCheck/>
                                     </div>
                                     <p className="text-sah-dark-2 text-[16px] font-medium ml-2">Various analysis options.</p>
                                 </li>
                                 <li className="flex items-center">
-                                    <div className="bg-sah-red  rounded-full h-4 w-4 text-[10px] flex items-center justify-center">
+                                    <div className="bg-sah-red  rounded-full min-w-4 h-4 w-4 text-[10px] flex items-center justify-center">
                                         <ProjectCheck/>
                                     </div>
                                     <p className="text-sah-dark-2 text-[16px] font-medium ml-2">Page Load (time, size, number of requests).</p>
                                 </li>
                                 <li className="flex items-center">
-                                    <div className="bg-sah-red  rounded-full h-4 w-4 text-[10px] flex items-center justify-center">
+                                    <div className="bg-sah-red  rounded-full min-w-4 h-4 w-4 text-[10px] flex items-center justify-center">
                                         <ProjectCheck/>
                                     </div>
                                     <p className="text-sah-dark-2 text-[16px] font-medium ml-2">Advance Data analysis operation.</p>
                                 </li>
                                 <li className="flex items-center">
-                                    <div className="bg-sah-red  rounded-full h-4 w-4 text-[10px] flex items-center justify-center">
+                                    <div className="bg-sah-red  rounded-full min-w-4 h-4 w-4 text-[10px] flex items-center justify-center">
                                         <ProjectCheck/>
                                     </div>
                                     <p className="text-sah-dark-2 text-[16px] font-medium ml-2">Various analysis options.</p>
                                 </li>
                             </ul>
                         </div>
-                        <div className="bg-sah-red p-[30px] pt-[20px] sticky top-[100px] w-full">
+                        <div className="bg-sah-red max-[640px]:px-4 p-[30px] pt-[20px] sticky top-[100px] w-full mt-8 xl:mt-0">
                             <h3 className="font-geist font-bold text-[24px] text-sah-white mb-[20px]">Project Info</h3>
                             <div className="" />
 
@@ -146,7 +146,7 @@ export default function ProjectDetailsPage() {
                                 { label: "End Date:", value: "14 October, 2024" },
                             ].map(({ label, value }, index) => (
                                 <div className="border-t border-sah-overlay-white-30" key={label}>
-                                    <div className="flex justify-between items-center py-[18px]">
+                                    <div className="flex flex-col gap-2 sm:gap-0 sm:flex-row justify-between items-center py-[18px]">
                                         <span className="font-semibold text-[16px] text-sah-white">{label}</span>
                                         <span className="font-normal text-[16px] text-sah-white">{value}</span>
                                     </div>
@@ -154,11 +154,11 @@ export default function ProjectDetailsPage() {
                             ))}
 
                             <div className="flex mt-[10px]">
-                                <ButtonModern link="#" label="Discuss Your Project" variant="solid" class_name="bg-sah-black w-full justify-center gap-[15px]" />
+                                <ButtonModern link="#" label="Discuss Your Project" variant="solid" class_name="bg-sah-black w-full justify-center gap-[15px] max-[1536px]:text-[14px] max-[640px]:text-[13px] max-[640px]:px-3" />
                             </div>
                         </div>
                     </div>
-                    <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 mt-9">
+                    <div className="grid  md:grid-cols-3 grid-cols-1 gap-6 mt-9">
                         {images.map((img) => (
                         <div
                             key={img.id}
@@ -185,7 +185,7 @@ export default function ProjectDetailsPage() {
                         ))}
                     </div>
                     <p className="text-[16px] leading-normal text-sah-gray-2 mt-8">At tempus aenean sapien torquent sed diam class efficitur mus morbi eros dictum quam augue ac laor eet ligula libero mi commodo nibh hac fermentum orci ad pharetra consequat justo pellentesque vulputate malesuada dictumst fames interdum cursus an te tellus porta ullamcorper accumsan non eu adipiscing integer venenatis sagittis arcu curae finibus ridi culus aliquam velit lobortis senectus vitae sollicitudin sit consectetuer ultricies rutrum parturient pede nisi nascetur habitant netus quisque elementum inceptos nam felis penatibus feugiat</p>
-                    <div className="flex items-center justify-between border-t border-b border-sah-light-3 pt-4.5 pb-4.5 gap-4 mt-11">
+                    <div className="flex sm:flex-row flex-col items-center justify-between border-t border-b border-sah-light-3 pt-4.5 pb-4.5 gap-4 mt-11">
 
                         
                         <a href="#"
@@ -242,7 +242,7 @@ export default function ProjectDetailsPage() {
                         <button
                             aria-label="Previous image"
                             onClick={() => navigate(-1)}
-                            className="absolute left-0 sm:-left-14 z-10 w-11 h-11 flex items-center justify-center rounded-full border border-white/20 text-white/60 text-[24px] hover:text-white hover:bg-sah-red hover:border-sah-red transition-all duration-200 cursor-pointer"
+                            className="absolute left-15 xl:-left-14 max-[1024px]:hidden z-10 w-11 h-11 flex items-center justify-center rounded-full border border-white/20 text-white/60 text-[24px] hover:text-white hover:bg-sah-red hover:border-sah-red transition-all duration-200 cursor-pointer"
                         >
                             ←
                         </button>
@@ -257,7 +257,7 @@ export default function ProjectDetailsPage() {
                         <button
                             aria-label="Next image"
                             onClick={() => navigate(1)}
-                            className="absolute right-0 sm:-right-14 z-10 w-11 h-11 flex items-center justify-center rounded-full border border-white/20 text-white/60 text-24px hover:text-white hover:bg-sah-red hover:border-sah-red transition-all duration-200 cursor-pointer"
+                            className="absolute right-15 xl:-right-14 max-[1024px]:hidden z-10 w-11 h-11 flex items-center justify-center rounded-full border border-white/20 text-white/60 text-24px hover:text-white hover:bg-sah-red hover:border-sah-red transition-all duration-200 cursor-pointer"
                         >
                             →
                         </button>
@@ -271,7 +271,7 @@ export default function ProjectDetailsPage() {
                     </div>
             
                     {/* Dot indicators */}
-                    <div className="fixed bottom-7 left-1/2 -translate-x-1/2 flex gap-1.5 z-50">
+                    <div className="fixed bottom-7 max-[1024px]:hidden left-1/2 -translate-x-1/2 flex gap-1.5 z-50">
                         {images.map((img) => (
                         <button
                             key={img.id}
